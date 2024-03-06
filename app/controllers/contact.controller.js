4542417933a7852eb7fde8b3bbe9a07d7214e547
Ngoc Khuu <khuuthibichngoc999@gmail.com>
2024-03-06 23:48:51 +0700

    connect database

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,27 +1,57 @@
-exports.create = (req, res) => {
-    res.send({message: "create handler"})
-}
+const ApiError = require("../api-error");
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.util");
 
-exports.findAll = (req, res) => {
-    res.send({message: "findAll handler"})
-}
+exports.create = async (req, res, next) => {
+  if (!req.body?.name) return next(new ApiError(400, "Name cannot be empty"));
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.create(req.body);
+    return res.send(document);
+  } catch (error) {
+    return next(
+      new ApiError(500, "An error occurred while creating the contact")
+    );
+  }
+};
+
+exports.findAll = async (req, res, next) => {
+  let documents = [];
+
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const { name } = req.body;
+    if (name) {
+      documents = await contactService.findByName(name);
+    } else {
+      documents = await contactService.find({});
+    }
+  } catch (error) {
+    console.log(error);
+    return next(
+      new ApiError(500, "An error occurred while retrieving contacts")
+    );
+  }
+
+  return res.send(documents);
+};
 
 exports.findOne = (req, res) => {
-    res.send({message: "findOne handler"})
-}
+  res.send({ message: "findOne handler" });
+};
 
 exports.update = (req, res) => {
-    res.send({message: "update handler"})
-}
+  res.send({ message: "update handler" });
+};
 
 exports.delete = (req, res) => {
-    res.send({message: "delete handler"})
-}
+  res.send({ message: "delete handler" });
+};
 
 exports.deleteAll = (req, res) => {
-    res.send({message: "deleteAll handler"})
-}
+  res.send({ message: "deleteAll handler" });
+};
 
 exports.findAllFavorite = (req, res) => {
-    res.send({message: "findAllFavorite handler"})
-}
+  res.send({ message: "findAllFavorite handler" });
+};
---
+++ app/services/contact.service.js
@@ -0,0 +1,44 @@
+const { ObjectId } = require("mongodb");
+
+class ContactService {
+  constructor(client) {
+    this.contact = client.db().collection("contacts");
+  }
+
+  extractContactData(payload) {
+    const contact = {
+      name: payload.name,
+      email: payload.email,
+      address: payload.address,
+      phone: payload.phone,
+      favorite: payload.favorite,
+    };
+
+    Objects.keys(contact).forEach(
+      (key) => contact[key] === undefined && delete contact[key]
+    );
+    return contact;
+  }
+
+  async create(payload) {
+    const contact = this.extractContactData(payload);
+    const result = await this.contact.findOneAndUpdate(
+      contact,
+      { $set: { favorite: contact.favorite === true } },
+      { returnDocument: "after", upsert: true }
+    );
+    return result.value;
+  }
+
+  async find(filter) {
+    const cursor = await this.contact.find(filter);
+    return await cursor.toArray();
+  }
+  async findByName(name) {
+    return await this.find({
+      name: { $regex: new RegExp(name), $options: "i" },
+    });
+  }
+}
+
+module.exports = ContactService;
